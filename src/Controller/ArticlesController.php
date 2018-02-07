bafd44c5160cc3080084b04a28b1061617cf146a
anastasionico <nico@anastasionico.uk>
2018-02-07 16:45:03 +0000

    Creating the Articles Controller

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,103 @@
+<?php
+// src/Controller/ArticlesController.php
+
+namespace App\Controller;
+
+class ArticlesController extends AppController
+{
+	public function inizialize()
+	{
+		parent::inizialize();
+
+		$this->loadComponent('Paginator');
+		$this->loadComponent('flash');
+}
+
+	// 	By defining function index() in our ArticlesController, users can now access the logic there by requesting www.example.com/articles/index
+	public function index()
+	{
+		/* 
+			Our controller action is very simple. 
+		 	It fetches a paginated set of articles from the database, using the Articles Model that is automatically loaded via naming conventions
+		*/
+		$this->loadComponent('Paginator');
+		$articles = $this->Paginator->paginate($this->Articles->find());
+		
+		// 	It then uses set() to pass the articles into the Template
+		$this->set(compact('articles'));
+	}
+
+	public function view($slug = null)
+	{
+		/*
+			The method findBySlug is a cakePHP dynamic finder,
+			This method allows us to create a basic query that finds articles by a given slug. 
+			We then use firstOrFail() to either fetch the first record, or throw a NotFoundException
+		*/
+		$article = $this->Articles->findBySlug($slug)->firstOrFail();
+		$this->set(compact('article'));
+	}
+
+	public function add()
+	{
+		$article = $this->Articles->newEntity();
+		
+		if($this->request->is('post')){
+			
+			// If the HTTP method of the request was POST, try to save the data using the Articles model.
+			$article = $this->Articles->patchEntity($article, $this->request->getData());
+
+			$article->user_id = 1;
+
+			// If for some reason it doesn’t save, just render the view. This gives us a chance to show the user validation errors or other warnings.
+			if($this->Articles->save($article)){
+				$this->flash->success(__('Your Article has been saved'));
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Flash->error(__('Unable to add your article'));
+
+			
+		}
+		$this->set('article', $article);
+	}
+
+	public function edit($slug = null)
+	{
+		// This action first ensures that the user has tried to access an existing record
+		$article = $this->Articles->findBySlug($slug)->firstOrFail();
+		
+		// the action checks whether the request is either a POST or a PUT request.
+		if($this->request->is(['post', 'put'])){
+			// we use the POST/PUT data to update our article entity by using the patchEntity() method
+			$this->Articles->patchEntity($article, $this->request->getData());
+
+			if($this->Articles->save($article)){
+				$this->flash->success(__('Your article has been updated'));
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Flash->error(__('Unable to update your article'));
+		}
+
+		$this->set('article', $article);
+	}
+
+	public function delete($slug = null)
+	{
+		// If the user attempts to delete an article using a GET request, allowMethod() will throw an exception.
+		$this->allowMethod('post','delete');
+
+		$articles = $this->Articles->findBySlug($slug)->firstOrFail();
+	
+		if($this->Articles->delete($article)){
+			$this->Flash->success(__('The {0} article has been deleted.', $article->title));
+			return $this->redirect(['action' => 'index']);
+		}
+	}
+
+
+
+        	
+    	    
+    
+
+}
--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,6 +3,8 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table
 {
@@ -11,4 +13,26 @@ class ArticlesTable extends Table
         // Timestamp behaviour automatically populate the created and modified columns of our table
         $this->addBehavior('Timestamp');
     }
+
+    public function beforeSave($event, $entity, $options)
+	{
+	    if ($entity->isNew() && !$entity->slug) {
+	        $sluggedTitle = Text::slug($entity->title);
+	        // trim slug to maximum length defined in schema
+	        $entity->slug = substr($sluggedTitle, 0, 191);
+	    }
+	}
+
+	public function validationDefault(Validator $validator)
+	{
+	    // The validationDefault() method tells CakePHP how to validate your data when the save() method is called. 
+	    $validator->notEmpty('title')
+	        ->minLength('title', 10)
+	        ->maxLength('title', 255)
+
+	        ->notEmpty('body')
+	        ->minLength('body', 10);
+
+	    return $validator;
+	}
 }
